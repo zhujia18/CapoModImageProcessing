d483d63a19ad69a704c90efc097458dbc2ae20ba
Jasper <>
2022-12-02 21:47:17 +0800

    update add https and domain and style processing

--- js/src/forum/replaceKeywords.js
+++ js/src/forum/replaceKeywords.js
@@ -1,53 +1,100 @@
-/*
- * This file is part of justoverclock/flarum-ext-keywords.
- *
- * Copyright (c) 2021 Marco Colia.
- * Special thanks to Askvortsov
- * https://flarum.it
- *
- * For the full copyright and license information, please view the LICENSE.md
- * file that was distributed with this source code.
- */
-
 import app from 'flarum/app';
 
-
-export default function () {
+export default function() {
   const post = this.attrs.post;
 
-  const mappings = {
+  const domainReplaceMappings = {
+    "cdn.jsdelivr.net": "testingcf.jsdelivr.net"
+  }
+
+  const imageStyleMappings = {
     "image.capomod.87rc.net": "!webp"
   }
 
-  if (mappings === {}) return;
-  if (Object.keys(mappings).length === 0) return;
+  const httpsMappings = {
+    "image.capomod.87rc.net": "https://"
+  }
 
-  this.attrs.post.data.attributes.contentHtml = post.contentHtml().replace(/<img [^>]*src=['"]([^'"]+)[^>]*>/gi, function (match, capture) {
-    // console.log(match);
-    // console.log(capture);
-    var domain = capture.split('/');
-    if (domain[2]) {
-        domain = domain[2];
-        let fixKey = mappings[domain.toLowerCase()];
-        let needDeal = confirmEnding(capture, fixKey)
-          if (fixKey && needDeal) {
-            let newImg = match.replace(capture,capture+fixKey)
-            return newImg;
-          } else {
-            return match
-          }
-    }else{
-        return match
-    }
-  });
-  
+  this.attrs.post.data.attributes.contentHtml = post.contentHtml()
+    .replace(/<img [^>]*src=['"]([^'"]+)[^>]*>/gi, function(match, capture) {
+      // console.log(match);
+      // console.log(capture);
+
+      //解析域名
+      let url = parseURL(capture)
+      var hostname = url.hostname
+      //处理图片域名
+      if (domainReplaceMappings.hasOwnProperty(hostname)) {
+        let newHostname = domainReplaceMappings[hostname.toLowerCase()];
+        let newCapture = capture.replace(hostname, newHostname)
+        match = match.replace(capture, newCapture)
+        capture = newCapture
+        hostname = newHostname
+      }
+
+      //处理图片样式
+      if (imageStyleMappings.hasOwnProperty(hostname)) {
+        let style = imageStyleMappings[hostname.toLowerCase()];
+        if (!confirmEnding(capture, style)) {
+          let newCapture = capture + style
+          match = match.replace(capture, capture + style)
+          capture = newCapture
+        }
+      }
+
+      //处理图片SSL
+      if (httpsMappings.hasOwnProperty(hostname)) {
+        let protocol = httpsMappings[hostname.toLowerCase()];
+        if (!confirmBeginning(capture, protocol)) {
+          match = match.replace("http://" + hostname, protocol + hostname)
+        }
+      }
+
+      return match
+    });
 }
 
-function confirmEnding(str, target) {
-  let strLen = str.length;
-  let targetLen = target.length;
-  if(str.substring(strLen-targetLen) == target){
-    return true;
+function parseURL(href) {
+  let url = new URL(href)
+  let params = new URLSearchParams(url.searchParams)
+  let obj = new Object()
+  Object.fromEntries(params)
+  obj.protocol = url.protocol
+  obj.hostname = url.hostname
+  obj.port = url.port
+  obj.pathname = url.pathname
+  obj.search = params
+  obj.hash = url.hash
+  return obj;
+}
+
+function confirmBeginning(string, target) {
+  return string.startsWith(target)
+}
+
+function confirmEnding(string, target) {
+  return string.endsWith(target);
+}
+
+function processingDomain(hostname, match, capture) {
+  let newHostname = domainReplaceMappings[hostname.toLowerCase()];
+  let newCapture = capture.replace(hostname, newHostname)
+  match = match.replace(capture, newCapture)
+  return match
+}
+
+function processingStyle(hostname, match, capture) {
+  let style = imageStyleMappings[hostname.toLowerCase()];
+  if (!confirmEnding(capture, style)) {
+    match = match.replace(capture, capture + style)
   }
-  return false;
- }
+  return match
+}
+
+function processingHTTPS(hostname, match, capture) {
+  let protocol = httpsMappings[hostname.toLowerCase()];
+  if (!confirmBeginning(capture, protocol)) {
+    match = match.replace("http://" + hostname, protocol + hostname)
+  }
+  return match
+}
